f164686601d121b2c039e1cf151aa9ac455c5f4f
Robert Plante <robertdplante@gmail.com>
2025-02-07 05:37:16 -0500

    Fixing up nvim-cmp bindings

--- nvim/init.lua
+++ nvim/init.lua
@@ -464,8 +464,8 @@ require("lazy").setup({
     -- LSP-based code-completion
     {
         "hrsh7th/nvim-cmp",
-        -- load cmp on InsertEnter
-        event = "InsertEnter",
+        -- load cmp in appropriate contexts
+        event = { "InsertEnter", "CmdlineEnter" },
         -- these dependencies will only be loaded when cmp loads
         -- dependencies are always lazy-loaded unless specified otherwise
         dependencies = {
@@ -484,13 +484,56 @@ require("lazy").setup({
                     end,
                 },
                 mapping = cmp.mapping.preset.insert({
+                    ['<Down>'] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), {'i'}),
+                    ['<Up>'] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), {'i'}),
+                    ['<C-n>'] = cmp.mapping({
+                        c = function()
+                            if cmp.visible() then
+                                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                            else
+                                vim.api.nvim_feedkeys(t('<Down>'), 'n', true)
+                            end
+                        end,
+                        i = function(fallback)
+                            if cmp.visible() then
+                                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                            else
+                                fallback()
+                            end
+                        end
+                    }),
+                    ['<C-p>'] = cmp.mapping({
+                        c = function()
+                            if cmp.visible() then
+                                cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+                            else
+                                vim.api.nvim_feedkeys(t('<Up>'), 'n', true)
+                            end
+                        end,
+                        i = function(fallback)
+                            if cmp.visible() then
+                                cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+                            else
+                                fallback()
+                            end
+                        end
+                    }),
                     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-e>'] = cmp.mapping.abort(),
                     -- Accept currently selected item.
                     -- Set `select` to `false` to only confirm explicitly selected items.
-                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                    ['<Tab>'] = cmp.mapping({
+                        i = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+                        c = function(fallback)
+                            if cmp.visible() then
+                                cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })
+                            else
+                                fallback()
+                            end
+                        end
+                    }),
                 }),
                 sources = cmp.config.sources({
                     { name = 'nvim_lsp' },
