27f996b1a05c6b00c6f7ee4d83f9de406882667e
Robert Plante <robertdplante@gmail.com>
2025-05-22 12:42:47 -0400

    Reusing open windows when LSP jumping

--- nvim/init.lua
+++ nvim/init.lua
@@ -429,8 +429,12 @@ require("lazy").setup({
                     -- Buffer local mappings.
                     -- See `:help vim.lsp.*` for documentation on any of the below functions
                     local opts = { buffer = ev.buf }
-                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                    vim.keymap.set('n', 'gD', function()
+                        vim.lsp.buf.declaration { reuse_win = true }
+                    end, opts)
+                    vim.keymap.set('n', 'gd', function()
+                        vim.lsp.buf.definition { reuse_win = true }
+                    end, opts)
                     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
                     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
                     vim.keymap.set('n', 'gh', vim.lsp.buf.typehierarchy, opts)
