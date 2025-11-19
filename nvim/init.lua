58adaf621a02e738a9baefaec31c9d1b13238d8c
Robert Plante <RobertDPlante@gmail.com>
2025-11-18 20:55:26 -0500

    Adding typescript LSP

--- nvim/init.lua
+++ nvim/init.lua
@@ -419,6 +419,11 @@ require("lazy").setup({
                 vim.lsp.enable('typst_lsp')
             end
 
+            -- Typescript LSP Server
+            if vim.fn.executable('typescript-language-server') == 1 then
+                vim.lsp.enable('ts_ls')
+            end
+
             -- Global mappings.
             -- See `:help vim.diagnostic.*` for documentation on any of the below functions
             vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
